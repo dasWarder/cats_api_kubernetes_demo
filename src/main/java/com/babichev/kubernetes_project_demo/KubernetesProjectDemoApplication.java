6b2f5bea702add68f87ea73670dbe6065050c086
dasWarder <Al.94_94@mail.ru>
2021-05-10 16:00:45 +0300

    Add docker compose and changelogs for db migration, add entity

---
+++ src/main/java/com/babichev/kubernetes_project_demo/KubernetesProjectDemoApplication.java
@@ -0,0 +1,13 @@
+package com.babichev.kubernetes_project_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesProjectDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubernetesProjectDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/babichev/kubernetes_project_demo/entity/Cat.java
@@ -0,0 +1,31 @@
+package com.babichev.kubernetes_project_demo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "cats-api")
+@Data
+@NoArgsConstructor
+public class Cat {
+    @Id
+    private String id;
+
+    @Column
+    private String name;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    
+}
